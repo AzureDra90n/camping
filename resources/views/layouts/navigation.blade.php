ced12ce026c7959ad3ce7c692272125a3b819839
Arif Rachman Husen <arif.rachman@sicepat.com>
2021-07-26 20:28:35 +0700

    header and footer

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,41 +1,29 @@
-{{-- <div class="container">
-  <header class="navbar fixed-top d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom bg-dark text-white">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none text-white">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-      <span class="fs-4">CAMPING FUN</span>
-    </a>
-
-    <ul class="nav nav-pills">
-      <li class="nav-item"><a class="nav-link{{ request()->is('/') ? ' active' : '' }}" aria-current="page" href="/">Home</a></li>
-      <li class="nav-item"><a class="nav-link{{ request()->is('admin') ? ' active' : '' }}" href="/admin">Admin</a></li>
-      <li class="nav-item"><a class="nav-link{{ request()->is('customer') ? ' active' : '' }}" href="/customer">Customer</a></li>
-      <li class="nav-item"><a class="nav-link{{ request()->is('about') ? ' active' : '' }}" href="/about">About Us</a></li>
-    </ul>
-  </header>
-</div> --}}
-
 <header class="sticky-top p-3 bg-dark text-white">
   <div class="container">
-    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-end">
-      <a href="/" class="d-flex justify-content-center align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-        <span class="fs-4">CAMPING FUN</span>
-      </a>
+    <div class="d-flex flex-wrap align-items-center justify-content-md-between col-12">
+      <div class="d-flex justify-content-md-around">
+        <div>
+          <a href="/" class="d-flex justify-content-center align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+            <span class="fs-4">CAMPING FUN</span>
+          </a>
+          
+        </div>
 
-      <ul class="nav col-12 col-lg-auto me-lg-auto mb-4 mb-md-0 justify-content-center">
-        <li><a aria-current="page" href="/" class="nav-link px-2 text-white{{ request()->is('/') ? ' active' : '' }}">Home</a></li>
-        <li><a href="/admin" class="nav-link px-2 text-white{{ request()->is('admin') ? ' active' : '' }}">Price List</a></li>
-        <li><a href="/customer" class="nav-link px-2 text-white{{ request()->is('customer') ? ' active' : '' }}">About Us</a></li>
-        <li><a href="/about" class="nav-link px-2 text-white{{ request()->is('about') ? ' active' : '' }}">FAQs</a></li>
-      </ul>
-
-      <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-        <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-      </form>
-
-      <div class="text-end">
-        <button type="button" class="btn btn-outline-light me-2">Sign-in</button>
-        <button type="button" class="btn btn-outline-warning me-2">Sign-Up</button>
+      </div>  
+      <div class="d-flex justify-content-md-end">
+        <div>
+          <ul class="nav col-12 col-lg-auto me-lg-auto mb-4 mb-md-0 justify-content-center">
+            <li><a aria-current="page" href="/" class="nav-link px-2 text-white{{ request()->is('/') ? ' active' : '' }}">Home</a></li>
+            <li><a href="/admin" class="nav-link px-2 text-white{{ request()->is('admin') ? ' active' : '' }}">Price List</a></li>
+            <li><a href="/customer" class="nav-link px-2 text-white{{ request()->is('customer') ? ' active' : '' }}">About Us</a></li>
+            <li><a href="/about" class="nav-link px-2 text-white{{ request()->is('about') ? ' active' : '' }}">FAQs</a></li>
+          </ul>
+        </div>
+        <div class="text-end">
+          <button type="button" class="btn btn-outline-light me-2">Sign-in</button>
+          <button type="button" class="btn btn-outline-warning me-2">Sign-Up</button>
+        </div>
       </div>
     </div>
   </div>
