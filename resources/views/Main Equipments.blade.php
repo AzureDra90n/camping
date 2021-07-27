33a6650135f8a71db4679f377bc44659e8105b89
AzureDra90n <vendetta10112540@gmail.com>
2021-07-28 00:53:56 +0700

    update-controller-card-view

--- resources/views/Main Equipments.blade.php
+++ resources/views/Main Equipments.blade.php
@@ -9,10 +9,31 @@
                 Main Equipments
             </h1>
         </div>
-        
-        @foreach ($dataBrg as $dBrg)
-            {{ $dBrg->Nama_Brg  }}
-        @endforeach
+
+        <div class="col d-flex justify-content-center flex-wrap">
+            @foreach ($dataBrg as $dBrg)
+            <div class="col d-flex justify-content-center mt-5">
+
+                <div class="card border-dark" style="width: 18rem;">
+
+                    <img src="{{ $dBrg->Foto_Brg }}" class="card-img-top" alt="...">
+
+                    <div class="card-body">
+                      <h5 class="card-title">{{ $dBrg->Nama_Brg  }}</h5>
+                      <p class="card-text">Merk: {{ $dBrg->Merk_Brg }}</p>
+                      <p class="card-text">Jenis: {{ $dBrg->Jenis_Brg }}</p>
+                      <p class="card-text">{{ $dBrg->Harga_Sewa }}</p>
+
+                      <a href="#" class="btn btn-outline-dark">Detail</a>
+                      <a href="#" class="btn btn-outline-warning">Add to Rent List</a>
+                    </div>
+
+                </div>
+
+            </div>
+            @endforeach
+            
+        </div>
 
 
     </div>
