4c0fd1ed00f252b22e82a56a65d34b28b953333d
AzureDra90n <vendetta10112540@gmail.com>
2021-07-29 20:54:18 +0700

    update-query-multiple-where0rwhere

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,6 +11,13 @@ class MainController extends Controller
     {
         $dataBrg = DB::table('tbl_barang')
             ->where('Jenis_Brg', '=', 'Tent')
+            ->orWhere(function ($query) {
+                $query->where('Jenis_Brg', '=', 'Flysheet')
+                    ->orwhere('Jenis_Brg', '=', 'Mattress')
+                    ->orWhere('Jenis_Brg', '=', 'Carrier Backpack')
+                    ->orWhere('Jenis_Brg', '=', 'Sleeping Bag')
+                    ->orWhere('Jenis_Brg', '=', 'Trekking Shoes');
+            })
             ->get();
 
         return view('Main Equipments', compact('dataBrg'));
