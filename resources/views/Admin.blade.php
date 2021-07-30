4762914c7078964866149e4a84db31c2da28dde6
AzureDra90n <vendetta10112540@gmail.com>
2021-07-30 16:20:20 +0700

    update-pre-auth-create

--- resources/views/Admin.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'CAMPING FUN | Price List')
-
-@section('content')
-    <div class="container">
-        Daftar Harga
-    </div>
-@endsection
--- resources/views/Customer.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'CAMPING FUN | Customer')
-
-@section('content')
-    <div class="container">
-        Customer
-    </div>
-@endsection
